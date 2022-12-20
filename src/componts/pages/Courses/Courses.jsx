c7642d06707f7d017ce21da5b8a8a27a3a8190b1
unknown <raihanarif778@gmail.com>
2022-12-20 23:54:59 +0600

    design changes

--- src/componts/pages/Courses/Courses.jsx
+++ src/componts/pages/Courses/Courses.jsx
@@ -12,7 +12,7 @@ const Courses = () => {
   return (
     <div className="relative">
       <div className="grid grid-cols-6 gap-3  ">
-        <div className=" fixed top-12 right-0 lg:col-span-2  mx-auto col-span-6 ">
+        <div className=" lg:fixed lg:top-12 lg:right-0 lg:col-span-2  mx-auto col-span-6 ">
           <RightSideNav></RightSideNav>
         </div>
         <div className="md:col-span-6 lg:col-span-4 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 col-span-6">
